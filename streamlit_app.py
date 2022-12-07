e8f693c30d51a803d116185025da3c5edad0ae6f
sfplynch1 <119956552+sfplynch1@users.noreply.github.com>
2022-12-07 08:20:35 -0700

    Update streamlit_app.py
    add picklist

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,7 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.dataframe(my_fruit_list)
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
